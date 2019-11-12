4f5a00bf6b68dd24977c33d354c493f7689f14dc
Myles Becker <mylesbecker1509@gmail.com>
2019-11-12 04:47:53 -0700

    changed terminal emulator to tilix, and trying to get postchroot art to show up.

--- installchroot.sh
+++ installchroot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+wget https://raw.githubusercontent.com/myles1509/CAIS/master/peng2.txt
 cat peng2.txt
 echo
 read -p 'Root Password: ' RootPass
@@ -14,7 +15,7 @@ echo $RootPass
 ln -s /usr/share/zoneinfo/America/Boise /etc/localtime
 mkinitcpio -p linux
 pacman -S --noconfirm archlinux-keyring
-pacman -Syu --noconfirm grub efibootmgr lightdm lightdm-gtk-greeter lightdm-webkit2-greeter i3-gaps i3blocks i3status terminator os-prober vim wpa_supplicant wireless_tools networkmanager nano plasma sudo chromium git openssh wget
+pacman -Syu --noconfirm grub efibootmgr lightdm lightdm-gtk-greeter lightdm-webkit2-greeter i3-gaps i3blocks i3status tilix os-prober vim wpa_supplicant wireless_tools networkmanager nano plasma sudo chromium git openssh wget
 mkdir /boot/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 grub-install /dev/sda
