b67093abb0e797816450e0914756d9e9af3303f1
myles <mylesbecker1509@gmail.com>
2019-11-19 16:16:07 -0700

    spaceship prompt and fixes

--- installpost.sh
+++ installpost.sh
@@ -1,9 +1,14 @@
 wget https://raw.githubusercontent.com/myles1509/CAIS/master/elephant.txt
 wget https://raw.githubusercontent.com/myles1509/CAIS/master/programs.sh
+wget https://raw.githubusercontent.com/myles1509/CAIS/master/pacmanupdate.sh
+wget https://raw.githubusercontent.com/myles1509/CAIS/master/gitsetup.sh
+wget https://raw.githubusercontent.com/myles1509/CAIS/master/zshinstall.sh
+wget https://raw.githubusercontent.com/myles1509/CAIS/master/zshrc
 cat elephant.txt
 chmod +x programs.sh
 chmod +x pacmanupdate.sh
 chmod +x gitsetup.sh
+chmod +x zshinstall.sh
 ./pacmanupdate.sh
 sudo pacman -Syu --noconfirm go
 git clone https://aur.archlinux.org/yay.git
@@ -14,4 +19,4 @@ rm -rf yay
 yay -Syyu
 ./gitsetup.sh
 ./programs.sh
-
+./zshinstall.sh
