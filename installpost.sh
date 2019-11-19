451cf6112bb6e2a0929ea44e8a37e9f47d8f37d2
myles <mylesbecker1509@gmail.com>
2019-11-19 15:28:17 -0700

    added git setup idea

--- installpost.sh
+++ installpost.sh
@@ -2,10 +2,15 @@ wget https://raw.githubusercontent.com/myles1509/CAIS/master/elephant.txt
 wget https://raw.githubusercontent.com/myles1509/CAIS/master/programs.sh
 cat elephant.txt
 chmod +x programs.sh
+chmod +x pacmanupdate.sh
+chmod +x gitsetup.sh
+./pacmanupdate.sh
 sudo pacman -Syu --noconfirm go
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
 cd ..
 rm -rf yay
+./gitsetup.sh
 ./programs.sh
+
