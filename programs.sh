86b9316995b9899dc5df7a951e797f3948113ca6
Myles Becker <mylesbecker1509@gmail.com>
2019-12-02 15:50:42 -0700

    update

--- programs.sh
+++ programs.sh
@@ -1,2 +1,2 @@
-yay -Syu --noconfirm pug
-yay -S --noconfirm rtl8821ce-dkms-git spotify polybar zsh android-studio mailspring simplenote nerd-fonts-complete micro sublime-text-dev intellij-idea-community-edition-jre spicetify-cli spicetify-themes-git compton nitrogen dmenu rofi networkmanager-dmenu lxappearance
+yay -Syu --noconfirm
+yay -S --noconfirm rtl8821ce-dkms-git spotify polybar zsh mailspring simplenote nerd-fonts-complete spicetify-cli spicetify-themes-git compton nitrogen dmenu rofi networkmanager-dmenu lxappearance
