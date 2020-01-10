de62447edde52e691d292250c83f5e31737d900d
myles1509 <48069588+myles1509@users.noreply.github.com>
2020-01-09 20:03:26 -0700

    Yay & DKMS?

--- installchroot.sh
+++ installchroot.sh
@@ -7,7 +7,7 @@ echo $RootPass
 ln -s /usr/share/zoneinfo/America/Boise /etc/localtime
 mkinitcpio -p linux
 pacman -S --noconfirm archlinux-keyring
-pacman -Syu --noconfirm grub efibootmgr sddm i3-gaps i3blocks plasma i3status tilix os-prober vim wpa_supplicant wireless_tools networkmanager sudo chromium git openssh wget
+pacman -Syu --noconfirm grub efibootmgr sddm i3-gaps i3blocks plasma i3status tilix os-prober vim wpa_supplicant wireless_tools networkmanager sudo chromium git openssh wget bc linux-headers dkms go
 wget https://raw.githubusercontent.com/myles1509/CAIS/master/peng2.txt
 cat peng2.txt
 echo
@@ -26,5 +26,12 @@ echo "$User ALL=(ALL:ALL) ALL" >> /etc/sudoers
 echo "Defaults        env_reset,timestamp_timeout=120"
 cd /home/$User/
 sudo -u $User wget https://raw.githubusercontent.com/myles1509/CAIS/master/installpost.sh
+# ADDED YAY?
+sudo -u $User git clone https://aur.archlinux.com/yay.git
+cd yay/
+sudo -u $User makepkg -si
+cd ..
+rm -rf yay
+sudo -u $User yay -S rtl8821ce-dkms-git
 chmod +x installpost.sh
 exit
